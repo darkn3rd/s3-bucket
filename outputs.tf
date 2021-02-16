aa42cc3336bae2cd7f55954c024f5d1f7cccd145
Joaquin Menchaca <joaquin.it@gmail.com>
2021-02-16 15:40:10 -0800

    update output for terraform 0.12+

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "bucket_id" {
-  value       = "${join("", aws_s3_bucket.default.*.id)}"
+  value       = join("", aws_s3_bucket.default.*.id)
   description = "Bucket Name (aka ID)"
 }
 
 output "bucket_arn" {
-  value       = "${join("", aws_s3_bucket.default.*.arn)}"
+  value       = join("", aws_s3_bucket.default.*.arn)
   description = "Bucket ARN"
 }
 
